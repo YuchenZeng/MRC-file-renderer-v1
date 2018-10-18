2d401a20b149815e607af4810904c7077bdb309b
Aditya <aditya.earanky@gmail.com>
2018-10-18 13:30:47 -0400

    remove functions, modify index_v3

--- functions/returnRenderer.js
+++
@@ -1,9 +0,0 @@
-function returnRenderer(){
-    let renderer = new THREE.WebGLRenderer( { antialias: false, alpha: true } );
-     renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    return renderer;
- }
-export{
-    returnRenderer
-}
