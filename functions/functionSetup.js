02e5cda70e2e9bbaa5e57f400b910eebf6f10835
Aditya <aditya.earanky@gmail.com>
2018-10-18 12:47:11 -0400

    clean up code

--- functions/functionSetup.js
+++ functions/functionSetup.js
@@ -1,33 +1,3 @@
- /*
- function setupInset (container2,document,renderer2,scene2,camera2,axes2,camera) {
-    var insetWidth = 150; 
-    var insetHeight = 150;
-    container2 = document.getElementById('inset');
-    container2.width = insetWidth;
-    container2.height = insetHeight;
-
-    // renderer
-    renderer2 = new THREE.WebGLRenderer({alpha : true});
-    renderer2.setClearColor( 0x000000, 0 );
-    renderer2.setSize( insetWidth, insetHeight );
-    container2.appendChild( renderer2.domElement );
-
-    // scene
-    scene2 = new THREE.Scene();
-
-    // camera
-    camera2 = new THREE.PerspectiveCamera( 50, insetWidth / insetHeight, 1, 1000 );
-    camera2.up = camera.up; // important!
-
-    // axes
-    axes2 = new THREE.AxisHelper( 100 );
-    scene2.add( axes2 );
-
-}
-export  {
-    setupInset
-}
-*/
 function containerFun(){
     var insetWidth = 150; 
     var insetHeight = 150;
